28b9e03f8e6a386ad261fb2c7eca6637d58f8412
DysonBreakstone <121531791+DysonBreakstone@users.noreply.github.com>
2023-04-25 17:19:21 -0600

    final model testing

--- spec/models/holiday_spec.rb
+++ spec/models/holiday_spec.rb
@@ -21,5 +21,22 @@ RSpec.describe Holiday, type: :model do
       expect(Holiday.next_three.pluck(:date).to_s).to eq("[Mon, 29 May 2023, Mon, 19 Jun 2023, Tue, 04 Jul 2023]")
     end
   end
+
+  describe "instance methods" do
+    before do
+      test_data
+      @bulk_discount_1 = @merchant_1.bulk_discounts.create!(name: "Discount 1", discount: 10, threshold: 5)
+      @bulk_discount_2 = @merchant_1.bulk_discounts.create!(name: "Discount 2", discount: 20, threshold: 10)
+      @bulk_discount_3 = @merchant_2.bulk_discounts.create!(name: "Discount 3", discount: 30, threshold: 15)
+      @bulk_discount_4 = @merchant_2.bulk_discounts.create!(name: "Discount 4", discount: 40, threshold: 20)
+      HolidayBuilder.make_holidays
+    end
+
+    it "#has_discount?" do
+      expect(Holiday.first.has_discount?(@merchant_1)).to eq(false)
+      @bulk_discount_1.holiday_bulk_discounts.create!(holiday_id: Holiday.first.id)
+      expect(Holiday.first.has_discount?(@merchant_1)).to eq(true)
+    end
+  end
 end
 
